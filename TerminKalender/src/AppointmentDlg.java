ed9de7235fec20efc110dfad47fd1566e98db165
zwepab16 <zwepab16@htlkaindorf.at>
2018-10-01 12:13:21 +0200

    Der hoffentlich letzte Dialog

--- TerminKalender/src/AppointmentDlg.java
+++ TerminKalender/src/AppointmentDlg.java
@@ -15,18 +15,65 @@ private boolean ok=false;
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jTextField5 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        jTextField6 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        getContentPane().setLayout(new java.awt.GridLayout(7, 2));
+
+        jLabel1.setText("jLabel1");
+        getContentPane().add(jLabel1);
+
+        jTextField1.setText("jTextField1");
+        getContentPane().add(jTextField1);
+
+        jLabel2.setText("jLabel1");
+        getContentPane().add(jLabel2);
+
+        jTextField2.setText("jTextField1");
+        getContentPane().add(jTextField2);
+
+        jLabel3.setText("jLabel1");
+        getContentPane().add(jLabel3);
+
+        jTextField3.setText("jTextField1");
+        getContentPane().add(jTextField3);
+
+        jLabel5.setText("jLabel1");
+        getContentPane().add(jLabel5);
+
+        jTextField5.setText("jTextField1");
+        getContentPane().add(jTextField5);
+
+        jLabel4.setText("jLabel1");
+        getContentPane().add(jLabel4);
+
+        jTextField4.setText("jTextField1");
+        getContentPane().add(jTextField4);
+
+        jLabel6.setText("jLabel1");
+        getContentPane().add(jLabel6);
+
+        jTextField6.setText("jTextField1");
+        getContentPane().add(jTextField6);
+
+        jButton1.setText("jButton1");
+        getContentPane().add(jButton1);
+
+        jButton2.setText("jButton2");
+        getContentPane().add(jButton2);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -49,5 +96,19 @@ private boolean ok=false;
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
+    private javax.swing.JTextField jTextField5;
+    private javax.swing.JTextField jTextField6;
     // End of variables declaration//GEN-END:variables
 }

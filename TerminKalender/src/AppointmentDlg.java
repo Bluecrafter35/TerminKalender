483acabb52971baa6e79568a3b0627bae2ea5fc5
zwepab16 <zwepab16@htlkaindorf.at>
2018-10-01 12:17:12 +0200

    Dialog erweitert

--- TerminKalender/src/AppointmentDlg.java
+++ TerminKalender/src/AppointmentDlg.java
@@ -16,67 +16,93 @@ private boolean ok=false;
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        tfTag = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        tfMonat = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        tfJahr = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        jTextField5 = new javax.swing.JTextField();
+        tfStunde = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
+        tfMinute = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
-        jTextField6 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        tfText = new javax.swing.JTextField();
+        btÜbernehmen = new javax.swing.JButton();
+        btAbbrechen = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         getContentPane().setLayout(new java.awt.GridLayout(7, 2));
 
-        jLabel1.setText("jLabel1");
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel1.setText("Tag: ");
         getContentPane().add(jLabel1);
 
-        jTextField1.setText("jTextField1");
-        getContentPane().add(jTextField1);
+        tfTag.setText("5");
+        getContentPane().add(tfTag);
 
-        jLabel2.setText("jLabel1");
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel2.setText("Monat: ");
         getContentPane().add(jLabel2);
 
-        jTextField2.setText("jTextField1");
-        getContentPane().add(jTextField2);
+        tfMonat.setText("9");
+        getContentPane().add(tfMonat);
 
-        jLabel3.setText("jLabel1");
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel3.setText("Jahr: ");
         getContentPane().add(jLabel3);
 
-        jTextField3.setText("jTextField1");
-        getContentPane().add(jTextField3);
+        tfJahr.setText("2014");
+        getContentPane().add(tfJahr);
 
-        jLabel5.setText("jLabel1");
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel5.setText("Stunde: ");
         getContentPane().add(jLabel5);
 
-        jTextField5.setText("jTextField1");
-        getContentPane().add(jTextField5);
+        tfStunde.setText("8");
+        getContentPane().add(tfStunde);
 
-        jLabel4.setText("jLabel1");
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel4.setText("Minute: ");
         getContentPane().add(jLabel4);
 
-        jTextField4.setText("jTextField1");
-        getContentPane().add(jTextField4);
+        tfMinute.setText("45");
+        getContentPane().add(tfMinute);
 
-        jLabel6.setText("jLabel1");
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLabel6.setText("Text: ");
         getContentPane().add(jLabel6);
 
-        jTextField6.setText("jTextField1");
-        getContentPane().add(jTextField6);
+        tfText.setText("POS_ÜBUNG");
+        getContentPane().add(tfText);
 
-        jButton1.setText("jButton1");
-        getContentPane().add(jButton1);
+        btÜbernehmen.setText("übernehmen");
+        btÜbernehmen.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btÜbernehmenActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btÜbernehmen);
 
-        jButton2.setText("jButton2");
-        getContentPane().add(jButton2);
+        btAbbrechen.setText("abbrechen");
+        btAbbrechen.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btAbbrechenActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btAbbrechen);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btÜbernehmenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btÜbernehmenActionPerformed
+     ok=true;
+     this.dispose();
+    }//GEN-LAST:event_btÜbernehmenActionPerformed
+
+    private void btAbbrechenActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btAbbrechenActionPerformed
+     ok=false;
+     this.dispose();
+    }//GEN-LAST:event_btAbbrechenActionPerformed
 
   
     public static void main(String args[]) {
@@ -96,19 +122,19 @@ private boolean ok=false;
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton btAbbrechen;
+    private javax.swing.JButton btÜbernehmen;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
+    private javax.swing.JTextField tfJahr;
+    private javax.swing.JTextField tfMinute;
+    private javax.swing.JTextField tfMonat;
+    private javax.swing.JTextField tfStunde;
+    private javax.swing.JTextField tfTag;
+    private javax.swing.JTextField tfText;
     // End of variables declaration//GEN-END:variables
 }

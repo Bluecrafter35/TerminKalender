324e2ba7341b9e4a9e6ff6fb06a27a701c6033d2
goealb16 <goedl.alex10@gmail.com>
2018-10-01 11:44:25 +0200

    load und save Methode implementiert

--- TerminKalender/src/TerminBL.java
+++ TerminKalender/src/TerminBL.java
@@ -1,6 +1,16 @@
 
+import java.awt.HeadlessException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import javax.swing.AbstractListModel;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -37,4 +47,45 @@ public class TerminBL extends AbstractListModel<Termin>
         termine.remove(i);
         fireIntervalRemoved(this, 0,termine.size()-1);
     }
+    
+    public void save()throws Exception{
+        try {
+            JFileChooser chooser = new JFileChooser();
+            chooser.addChoosableFileFilter(new FileNameExtensionFilter("*.txt", "txt"));
+            File file = null;
+            int result = chooser.showSaveDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                file = chooser.getSelectedFile();
+            }
+
+            FileOutputStream fos = new FileOutputStream(file.getAbsolutePath());
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject((java.util.ArrayList) termine);
+            oos.flush();
+            fos.close();
+        } catch (HeadlessException | IOException e) {
+            throw e;
+        }
+    }
+
+    public void load() throws Exception{
+        termine.removeAll(termine);
+        try {
+            JFileChooser chooser = new JFileChooser();
+            chooser.addChoosableFileFilter(new FileNameExtensionFilter("*.txt", "txt"));
+            File file = null;
+            int result = chooser.showSaveDialog(null);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                file = chooser.getSelectedFile();
+            }
+            FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            termine = (ArrayList<Termin>) ois.readObject();
+            fis.close();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    
+    
 }
